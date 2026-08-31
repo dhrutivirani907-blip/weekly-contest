b319b93d393de80394faba8e3b895f2e52e7e5f5
dhrutivirani907-blip <dhrutivirani907@gmail.com>
2026-08-31 13:34:05 +0530

    Update userId.js

--- utils/userId.js
+++ utils/userId.js
@@ -1,9 +1,6 @@
-const { v4: uuidv4 } = require("uuid");
-
 function generateUserId() {
-
-    return "CRA-" + uuidv4().replace(/-/g, "").substring(0, 10).toUpperCase();
-
+    const randomNumber = Math.floor(10000000 + Math.random() * 90000000);
+    return `CRA-${randomNumber}`;
 }
 
 module.exports = generateUserId;
